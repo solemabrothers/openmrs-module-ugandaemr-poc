dde5ac3ed5ff92460ab844869b43e28a9ca0d2da
Lubwama Samuel <lubwamasamuel3@gmail.com>
2019-07-03 08:15:22 +0300

    Removig Empty Classes

--- api/src/main/java/org/openmrs/module/ugandaemrpoc/api/UgandaEMRPOCService.java
+++
@@ -1,7 +0,0 @@
-package org.openmrs.module.ugandaemrpoc.api;
-
-import org.openmrs.api.OpenmrsService;
-
-public abstract interface UgandaEMRPOCService extends OpenmrsService {
-	
-}
--- api/src/main/java/org/openmrs/module/ugandaemrpoc/api/db/UgandaEMRPOCDao.java
+++
@@ -1,5 +0,0 @@
-package org.openmrs.module.ugandaemrpoc.api.db;
-
-public abstract interface UgandaEMRPOCDao {
-	
-}
