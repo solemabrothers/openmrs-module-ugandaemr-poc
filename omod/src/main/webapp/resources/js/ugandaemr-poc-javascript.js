b52d71397bc7cc90f78a471307dbd694f9823e29
solemabrothers <ssevvume1994solema@gmail.com>
2019-07-05 17:32:33 +0300

    Standardize the form fields to match the format reccomended in the technical guide and seperate css files from the html forms.

--- omod/src/main/webapp/resources/js/ugandaemr-poc-javascript.js
+++ omod/src/main/webapp/resources/js/ugandaemr-poc-javascript.js
@@ -1,53 +1,53 @@
-function enable_disable(field, class_name_prefix, conditions, input_type) {
+    function enable_disable(field, class_name_prefix, conditions, input_type) {
 
-    var class_name = jq(field).attr("class");
+        var class_name = jq(field).attr("class");
 
-    var length = class_name.length;
+        var length = class_name.length;
 
-    var class_id = parseInt(class_name.substring(length - 1, length));
-    var childClass = "Child" + class_id;
+        var class_id = parseInt(class_name.substring(length - 1, length));
+        var childClass = "Child" + class_id;
 
-    var disable = true;
-    var requires = true;
+        var disable = true;
+        var requires = true;
 
-    var row = '[class^="' + class_name_prefix + '"][class*="' + childClass + '"]';
+        var row = '[class^="' + class_name_prefix + '"][class*="' + childClass + '"]';
 
-    var selected_value = null;
+        var selected_value = null;
 
-    if (input_type == "select") {
-        selected_value = jq(field).find(":selected").text().trim().toLowerCase();
-    }
-    else if (input_type == "hidden") {
-        selected_value = jq(field).find("input[type=hidden]").val().trim().toLowerCase();
-    }
-
-
-    if (eval(conditions)) {
-        disable = false;
-    }
-
-
-    jq(row).each(function () {
-        var group = jq(this);
-
-        if (class_name.indexOf('Child') == -1) {
-            /*group.find("input").attr("value", ""); */
-            /* group.find('select').prop("selectedIndex", 0);*/
-            group.find("input").attr("disabled", disable);
-            group.find('select').attr("disabled", disable);
-
-            if (disable) {
-                /* fade out the fields that are disabled */
-                group.find("input").fadeTo(250, 0.25);
-                group.find("select").fadeTo(250, 0.25);
-            } else {
-                /* remove the fade on the fields */
-                group.find("input").fadeTo(250, 1);
-                group.find("select").fadeTo(250, 1);
-            }
+        if (input_type == "select") {
+            selected_value = jq(field).find(":selected").text().trim().toLowerCase();
         }
-    });
-}
+        else if (input_type == "hidden") {
+            selected_value = jq(field).find("input[type=hidden]").val().trim().toLowerCase();
+        }
+
+
+        if (eval(conditions)) {
+            disable = false;
+        }
+
+
+        jq(row).each(function () {
+            var group = jq(this);
+
+            if (class_name.indexOf('Child') == -1) {
+                /*group.find("input").attr("value", ""); */
+                /* group.find('select').prop("selectedIndex", 0);*/
+                group.find("input").attr("disabled", disable);
+                group.find('select').attr("disabled", disable);
+
+                if (disable) {
+                    /* fade out the fields that are disabled */
+                    group.find("input").fadeTo(250, 0.25);
+                    group.find("select").fadeTo(250, 0.25);
+                } else {
+                    /* remove the fade on the fields */
+                    group.find("input").fadeTo(250, 1);
+                    group.find("select").fadeTo(250, 1);
+                }
+            }
+        });
+    }
 
 if (jQuery) {
     $(document).ready(function () {
